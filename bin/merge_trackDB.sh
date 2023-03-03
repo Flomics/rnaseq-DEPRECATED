6301d7a6e841c51b0dbf56f1f7d7e27b5d4c07c3
lcabus-flomics <lluc.cabus@flomics.com>
2023-03-03 12:03:40 +0100

    Fixed trackhubs paths

--- bin/merge_trackDB.sh
+++ bin/merge_trackDB.sh
@@ -3,7 +3,7 @@
 outdir=$1
 project_name=${outdir#"s3://flomics-data/"}
 
-s3_bucket_name="s3://flomics-public/RNAseq_pipeline_trackHubs/"$folder_name
+s3_bucket_name="s3://flomics-public/RNAseq_pipeline_trackHubs/"$project_name
 #Aggregate track_Db.txt files and uploads them to s3
 for file in *trackDb.txt
 do cat $file >> trackDb.txt
