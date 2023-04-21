b9d8ac0031417bbfa7838662927f51f32dac6da8
ctuni <c.tuni.dominguez@gmail.com>
2023-04-21 14:46:46 +0200

    update

--- bin/spike-in_qc.r
+++ bin/spike-in_qc.r
@@ -5,6 +5,14 @@ library(stringr)
 library(dplyr)
 
 # load featurecounts results
-featurecounts <- read.table(text = gsub(" ", "\t", readLines(args[1]))) #change this in pipeline with the path to bin
+featurecounts <- read.table(text = gsub(" ", "\t", readLines("/home/ctuni/Downloads/biotype_counts(3).tsv"))) #change this in pipeline with the path to bin
 colnames(featurecounts) <- featurecounts[1,]
 featurecounts <- featurecounts[-1,]
+rownames(featurecounts) <- featurecounts[,1]
+
+featurecounts <- featurecounts[ , -which(names(featurecounts) %in% c("biotype"))]
+featurecounts_num <- mutate_all(featurecounts, function(x) as.numeric(x))
+
+featurecounts_t <- t(featurecounts_num)
+
+featurecounts_ordered <- featurecounts_t[order(rownames(featurecounts_t)),]
