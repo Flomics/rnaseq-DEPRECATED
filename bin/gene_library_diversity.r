b836037a23b0cfe9df394e7b0b9ad0b6321dbf7d
ctuni <c.tuni.dominguez@gmail.com>
2023-12-19 12:22:50 +0100

    modified aggregator

--- bin/gene_library_diversity.r
+++ bin/gene_library_diversity.r
@@ -2,18 +2,18 @@
 
 # read counts matrix
 input_file <- commandArgs(trailingOnly = TRUE)[1]
-count_matrix <- read.table(input_file, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+count_matrix <- read.table(input_file, header = TRUE, stringsAsFactors = FALSE, sep = '\t')
 
 column_names <- colnames(count_matrix)
 column_indices <- 3:ncol(count_matrix)
 
 # empty data frame to store the results
 result_table <- data.frame(sample = character(),
-                           "number_of_genes_contributing_to_1%_of_reads" = numeric(),
-                           "number_of_genes_contributing_to_5%_of_reads" = numeric(),
-                           "number_of_genes_contributing_to_10%_of_reads" = numeric(),
-                           "number_of_genes_contributing_to_50%_of_reads" = numeric(),
-                           "number_of_genes_contributing_to_80%_of_reads" = numeric(),
+                           'number_of_genes_contributing_to_1%_of_reads' = numeric(),
+                           'number_of_genes_contributing_to_5%_of_reads' = numeric(),
+                           'number_of_genes_contributing_to_10%_of_reads' = numeric(),
+                           'number_of_genes_contributing_to_50%_of_reads' = numeric(),
+                           'number_of_genes_contributing_to_80%_of_reads' = numeric(),
                            stringsAsFactors = FALSE, check.names = FALSE)
 
 percentages <- c(1, 5, 10, 50, 80)
@@ -23,7 +23,7 @@ for (col_index in column_indices) {
   col_name <- column_names[col_index]
   col_values <- count_matrix[, col_index]
 
-  # Calculate
+  # calculate
   genes_count <- sapply(percentages, function(percent) {
     threshold <- percent / 100 * sum(col_values)
     length(which(cumsum(col_values) <= threshold)) + 1
@@ -32,5 +32,5 @@ for (col_index in column_indices) {
   result_table[nrow(result_table) + 1, ] <- c(col_name, genes_count)
 }
 
-write.table(result_table, file = "genes_contributing_to_percentage_reads.tsv", sep = "\t", row.names = FALSE)
+write.table(result_table, file = 'genes_contributing_to_percentage_reads.tsv', sep = '\t', row.names = FALSE)
 
