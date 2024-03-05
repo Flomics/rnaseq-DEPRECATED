cf290885d57776de255ee0d638d494c45aad28c7
ctuni <c.tuni.dominguez@gmail.com>
2024-03-05 12:11:26 +0100

    added shebang

--- bin/tsv_to_yaml.py
+++ bin/tsv_to_yaml.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import csv
 import yaml
 import sys
@@ -7,16 +9,13 @@ if len(sys.argv) != 3:
     print("Usage: script.py input_file.tsv output_file.yml")
     sys.exit(1)
 
-# The first command-line argument is the TSV file path
 tsv_file = sys.argv[1]
-# The second command-line argument is the YAML file path
 yaml_file = sys.argv[2]
 
 with open(tsv_file, mode='r', newline='') as file:
     # Read the TSV file
     reader = csv.DictReader(file, delimiter='\t')
 
-    # Assuming there's only one data row
     for row in reader:
         data_row = dict(row)
         break
@@ -24,7 +23,6 @@ with open(tsv_file, mode='r', newline='') as file:
 # Convert the dictionary to a YAML string
 yaml_str = yaml.dump(data_row, sort_keys=False)
 
-# Saving the YAML string to a file specified by command line argument
 with open(yaml_file, 'w') as f:
     f.write(yaml_str)
 
