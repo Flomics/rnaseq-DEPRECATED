f1a157f0869aef628aaa398eeb9bbf2f026cef7d
Julien Lagarde <julienlag@gmail.com>
2024-04-08 15:57:54 +0200

    empty master branch

--- bin/make_files_public.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-multiqc_report=$1
-flomics_QC_report_table=$2
-project=$3
-uuid=$4
-s3_bucket_name="s3://flomics-public/RNAseq_pipeline/$project/$uuid/"
-
-aws s3 cp $multiqc_report $s3_bucket_name --acl public-read
-aws s3 cp $flomics_QC_report_table $s3_bucket_name --acl public-read
--- bin/umitools_dedup_rate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-lines_dedup_bam=$(samtools view -F 4 $1| cut -f1 | sort | uniq | wc -l)
-lines_bam=$(samtools view -F 4 $2| cut -f1 | sort | uniq  | wc -l)
-
-echo -e "Number of mapped unique molecules\tPercentage of unique molecules" >${3}_UMI_dedup.tsv
-echo -en "$lines_dedup_bam\t" >>${3}_UMI_dedup.tsv
-echo "scale=3; $lines_dedup_bam / $lines_bam *100" | bc >>${3}_UMI_dedup.tsv
